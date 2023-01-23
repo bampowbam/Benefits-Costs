c8d36c664ae68bda39b758a58a3c4613b1502d73
FlashEducation <lbalogun16@gmail.com>
2023-01-23 01:06:00 -0600

    initial commit

---
+++ App/Models/Dependent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Paylocity.Models
+{
+    public class Dependent
+    {
+        public Dependent()
+        {
+        }
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public Types Type { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid EmployeeID { get; set; }
+        public Employee Employee { get; set; }
+
+        public enum Types
+        {
+            Spouse=0,
+            Child=1,
+            Parent=2
+        }
+
+    }
+}
